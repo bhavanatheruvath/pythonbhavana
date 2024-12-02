011916b5de5e083ad73f3367fa184dd943ddde70
bhavanatheruvath <bhavanatheruvath2255@gmail.com>
2024-12-02 12:01:09 +0530

    02-12-24

---
+++ timeoverloading.py
@@ -0,0 +1,23 @@
+class Time:
+    def __init__(self,hours=0,mins=0,secs=0):
+        self.hours=hours
+        self.mins=mins
+        self.secs=secs
+    def __add__(self,time2):
+        secs=(self.secs+time2.secs)%60
+        mins=(self.secs+time2.secs)//60+(self.mins+time2.mins)%60
+        hours=(self.mins+time2.mins)//60+(self.hours+time2.hours)%60
+        time3=Time(hours,mins,secs)    
+        return time3
+    def getTime(self):
+        print(str(self.hours)+":"+str(self.mins)+":"+str(self.secs))
+
+h1,m1,s1=map(int,input("Enter the hours,minutes,seconds of time 1: ").split())
+h2,m2,s2=map(int,input("Enter the hours,minutes,seconds of time 2: ").split())  
+
+t1=Time(h1,m1,s1)
+t2=Time(h2,m2,s2)
+t3=t1+t2
+t1.getTime()
+t2.getTime()
+t3.getTime()
