25b349d317e252b18d1407cc9ccafd14d244f25f
bhavanatheruvath <99276236+bhavanatheruvath@users.noreply.github.com>
2025-01-02 09:30:51 +0530

    Create evenlist.py

---
+++ evenlist.py
@@ -0,0 +1,8 @@
+numbers=[]
+s=int(input("Enter the size of the list: "))
+for i in range(s):
+    n=int(input(f"{i+1} element: "))
+    numbers.append(n)
+print(numbers)
+evennumbers=[x for x in numbers if x%2==0]
+print(evennumbers)
