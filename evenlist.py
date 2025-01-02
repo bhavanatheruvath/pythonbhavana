960216f8b9aed9bac58ff3fa0c31fd0fced12a34
bhavanatheruvath <99276236+bhavanatheruvath@users.noreply.github.com>
2025-01-02 09:33:31 +0530

    Update evenlist.py

--- evenlist.py
+++ evenlist.py
@@ -4,5 +4,5 @@ for i in range(s):
     n=int(input(f"{i+1} element: "))
     numbers.append(n)
 print(numbers)
-evennumbers=[x for x in numbers if x%2==0]
+evennumbers=[x for x in numbers if x%2!=0]
 print(evennumbers)
