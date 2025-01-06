1f4fd4bf8d85943415c2e3c8bcd2c377fb7cd761
bhavanatheruvath <99276236+bhavanatheruvath@users.noreply.github.com>
2025-01-07 04:41:55 +0530

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,4 +1,4 @@
-#Create a class Publisher (name). Derive class Book from Publisher with attributes title and 
+#Create a class Publisher (name). Derive class Book from Publisher with attributes title and
 author.Derive class Python from Book with attributes price and no_of_pages.Write a 
 program that displays information about a Python book.Use base class constructor invocation and method overriding.
 class Publisher:
