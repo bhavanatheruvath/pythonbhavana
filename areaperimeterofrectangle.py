54a5676ead125e4c2451c266712d9034a284544b
bhavanatheruvath <99276236+bhavanatheruvath@users.noreply.github.com>
2025-01-06 21:11:04 +0530

    Update areaperimeterofrectangle.py

--- areaperimeterofrectangle.py
+++ areaperimeterofrectangle.py
@@ -1,3 +1,5 @@
+#Create Rectangle class with attributes length and breadth and methods to find area and 
+perimeter. Compare two Rectangle objects by their area.
 class rectangle:
     def __init__(self,length,breadth):
         self.l=length
